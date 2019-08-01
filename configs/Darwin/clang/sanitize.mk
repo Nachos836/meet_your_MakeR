8814af34572fdcf69113ec01573f301106ca12f5
nachos836 <nachos836@gmail.com>
2019-08-01 22:13:20 +0300

    MacOS sanitize hotfix

--- configs/Darwin/clang/sanitize.mk
+++ configs/Darwin/clang/sanitize.mk
@@ -4,6 +4,8 @@ PROJ_CFLAGS	+=	-g3 \
 
 ifeq ($(ASAN),)
 	PROJ_CFLAGS	+=	-fsanitize="undefined,shift,shift-exponent,shift-base,integer-divide-by-zero,unreachable,vla-bound,null,signed-integer-overflow,bounds,alignment,float-divide-by-zero,float-cast-overflow,nonnull-attribute,returns-nonnull-attribute,bool,enum,pointer-overflow"
-else ifeq ($(ASAN),LEAKS)
+else ifeq ($(ASAN),ADDRESS)
 	PROJ_CFLAGS	+=	-fsanitize="address"
+else ifeq ($(ASAN),THREAD)
+	PROJ_CFLAGS	+=	-fsanitize="thread" -fPIE
 endif
